85149f6583a5a749c799b98d4f2aa57455115685
KairaAnton <semeshko.94@gmail.com>
2022-05-13 16:40:00 +0300

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.scss';
 import HomePage from './pages/HomePage';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <Routes>
+      <Route exect path='/' element={<HomePage />} />
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './reduxToolkit';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-  <Provider store = {store}>
-    <App />
+
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 );
